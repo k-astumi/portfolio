83420a0ece20f03573a24fe09c06fa848c0abd04
渥美香央梨 <chakapin16@gmail.com>
2021-12-24 18:01:46 +0900

    :memo: 不要ファイル削除

--- blog/wp-content/themes/laid-back/header.php
+++ blog/wp-content/themes/laid-back/header.php
@@ -10,6 +10,16 @@
 wp_head();
 
 ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215764963-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-215764963-1');
+</script>
+
 </head>
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
